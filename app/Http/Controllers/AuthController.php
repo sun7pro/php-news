c6551296d5e598dc46600edaf6074af18b44867f
haolt <lehaohust@gmail.com>
2020-12-26 08:11:50 +0700

    refs task/V/Logout --update-api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
 
@@ -28,8 +27,8 @@ class AuthController extends Controller
         ], 403);
     }
 
-    public function logout(Request $request)
+    public function logout()
     {
-        Auth::guard('web')->logout();
+        Auth::logout();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\AuthController;
 
 Route::group([
     'prefix' => 'v1',
